f9b3bedf66d09b43a00a81d4809c2e087ed62d9b
Uttamluffy <127734652+Uttamluffy@users.noreply.github.com>
2024-04-11 21:44:36 +0545

    assignment

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('signup/', views.user_signup, name='user_signup'),
     path('logout/', views.user_logout, name='user_logout'),
+    fndf
 ]
